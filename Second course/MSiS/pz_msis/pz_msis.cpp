9957ce4f684bfac3f39eee2ebee156e19c1cb48e
ElizVerk <verkovich2005@gmail.com>
2023-09-12 19:29:59 +0300

    msis

---
+++ Second course/MSiS/pz_msis/pz_msis.cpp
@@ -0,0 +1,27 @@
+﻿#include<ctime>
+#include <iostream>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    const int N = 100;
+    int arr[N];
+    int n, x;
+    cout << "input N: \n";
+    cin >> n;
+    cout << "ur array: \n";
+    for (int i = 0; i < n; i++) {
+
+        arr[i] = rand() % 20;
+        cout << arr[i] << " ";
+    }
+    float s = 0;
+    for (int i = 0; i < n; i++) {
+        s += arr[i] * arr[i];
+
+    }
+    cout << "\nsrednee arifm. = " << s / n;
+    cout << "\ntime = " << clock()/1000.0;
+    return 0;
+}
+
